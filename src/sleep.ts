10b7d222bd31bfc06c2e66a7e3369ec5d21f44ae
Mohammed Adekunle <adekunle3317@gmail.com>
2021-05-19 18:21:13 +0100

    Updated sleep function

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,7 +1,7 @@
-export default function sleep(seconds: number): Promise<void> {
+export default function sleep(seconds: number): Promise<string> {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve();
+            resolve(Date.now().toString());
         }, seconds);
     })
 }
